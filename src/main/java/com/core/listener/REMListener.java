74dfc69480194092f0d996c2ee03531747ce91d3
Yokesh V S <yokeshvsk@gmail.com>
2018-09-15 18:24:43 -0500

    REMListener Skeleton

--- src/main/java/com/core/listener/REMListener.java
+++ src/main/java/com/core/listener/REMListener.java
@@ -1,5 +1,9 @@
 package com.core.listener;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class REMListener {
 
+	private static final Logger LOGGER = LogManager.getLogger("REMListener");
 }
